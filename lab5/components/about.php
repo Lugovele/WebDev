8aab6a3768c0e49ff827e6a62c492c8be246813d
Lugovele <lu3la@yandex.ru>
2021-06-04 16:01:37 +0300

    вторая и третья лабораторная работа

---
+++ lab5/components/about.php
@@ -0,0 +1,9 @@
+<section class="about container">
+  <h2 class="section-title">Привет, я <?= $name ?></h2>
+  <img class="about-image" src="<?= $image ?>" width="147" height="147" alt="<?= $name ?>">
+  <div class="about-content">
+    <p>Хочу жить в радость.</p>
+    <p> Получается не всегда.</p>
+    <p> Стремлюсь.</p>
+  </div>
+</section>
---
+++ lab5/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Мой сайт-визитка';
+$name = 'Опознанный енот НЛО';
+$image = 'img/00_ufo_.jpg';
+$email = 'enot_opoznanniy@gmail.com';
+$phone = '+79178547214';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
