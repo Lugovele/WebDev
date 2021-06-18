774f2a6a1ee69e14a512679a0e78859aad879b89
Lugovele <lu3la@yandex.ru>
2021-06-18 21:53:12 +0300

    3

---
+++ lab10/skilles.js
@@ -0,0 +1,26 @@
+     
+function insert_skills(skills) {
+  const dl = document.createElement('dl');
+  dl.classList.add('skills-list');
+  skills.forEach(item => {
+    const dt = document.createElement('dt');
+    dt.textContent = item.name;
+    dt.classList.add(item.css_class);
+    const dd = document.createElement('dd');
+    dd.classList.add('level');
+    const div = document.createElement('div');
+    div.style.width = item.level + '%';
+    div.textContent = item.level;
+    dd.appendChild(div);
+    dl.append(dt);
+    dl.append(dd);
+  });
+  document.querySelector('.skills').append(dl);
+ }
+
+ fetch('db/skills.json')
+ .then(data => data.json())
+ .then(json => { 
+ insert_skills(json); 
+ })
+ .catch(() => console.error("упс, что-то пошло не так"));
